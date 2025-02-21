a5375527c0146ff4944a605fe2530dab2a897192
Meganathan <meganathan212003@gmail.com>
2025-02-21 17:26:05 +0530

    fix(user): fix the main ts file

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-
 import userRoutes from "./routes/user.routes";
 import express from "express";
 import dotenv from "dotenv";
@@ -9,13 +8,7 @@ dotenv.config();
 
 const app = express();
 
-
-import app from "./routes/user.routes";
-import dotenv from "dotenv";
-import cors from "cors";
-
 dotenv.config();
-
 
 const PORT = process.env.PORT || 8080;
 app.use(
@@ -35,4 +28,3 @@ app.use(middleWare);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
